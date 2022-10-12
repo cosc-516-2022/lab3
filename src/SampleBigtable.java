35011eb95aaa319a2d42ee555673bd902d43ebb5
Ramon Lawrence <ramon.lawrence@ubc.ca>
2022-10-11 18:02:16 -0700

    Lab 3

---
+++ src/SampleBigtable.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START bigtable_hw_imports]
+import com.google.api.gax.rpc.NotFoundException;
+import com.google.api.gax.rpc.ServerStream;
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
+import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.cloud.bigtable.data.v2.models.Query;
+import com.google.cloud.bigtable.data.v2.models.Row;
+import com.google.cloud.bigtable.data.v2.models.RowCell;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+// [END bigtable_hw_imports]
+
+/**
+ * An example of using Google Cloud Bigtable.
+ *
+ * <p>This example is a very simple "hello world" application, that illustrates how to create a new
+ * table, write to the table, read the data back, and delete the table.
+ *
+ * <ul>
+ *   <li>create table
+ *   <li>read single row
+ *   <li>read table
+ *   <li>delete table
+ * </ul>
+ */
+public class SampleBigtable {
+
+  private static final String COLUMN_FAMILY = "cf1";
+  private static final String COLUMN_QUALIFIER_GREETING = "greeting";
+  private static final String COLUMN_QUALIFIER_NAME = "name";
+  private static final String ROW_KEY_PREFIX = "rowKey";
+  private final String tableId;
+  private final BigtableDataClient dataClient;
+  private final BigtableTableAdminClient adminClient;
+
+  public static void main(String[] args) throws Exception {
+
+    String projectId = "autoed";
+    String instanceId = "cosc516";
+    /* 
+    if (args.length != 2) {
+      System.out.println("Missing required project id or instance id");
+      return;
+    }
+    String projectId = args[0];
+    String instanceId = args[1];
+    */
+    SampleBigtable helloWorld = new SampleBigtable(projectId, instanceId, "testtable");
+    helloWorld.run();
+  }
+
+  public SampleBigtable(String projectId, String instanceId, String tableId) throws IOException {
+    this.tableId = tableId;
+
+    // [START bigtable_hw_connect]
+    // Creates the settings to configure a bigtable data client.
+    BigtableDataSettings settings =
+        BigtableDataSettings.newBuilder().setProjectId(projectId).setInstanceId(instanceId).build();
+
+    // Creates a bigtable data client.
+    dataClient = BigtableDataClient.create(settings);
+
+    // Creates the settings to configure a bigtable table admin client.
+    BigtableTableAdminSettings adminSettings =
+        BigtableTableAdminSettings.newBuilder()
+            .setProjectId(projectId)
+            .setInstanceId(instanceId)
+            .build();
+
+    // Creates a bigtable table admin client.
+    adminClient = BigtableTableAdminClient.create(adminSettings);
+    // [END bigtable_hw_connect]
+  }
+
+  public void run() throws Exception {
+    createTable();
+    writeToTable();
+    readSingleRow();
+    readSpecificCells();
+    readTable();
+    deleteTable();
+    close();
+  }
+
+  public void close() {
+    dataClient.close();
+    adminClient.close();
+  }
+
+  /** Demonstrates how to create a table. */
+  public void createTable() {
+    // [START bigtable_hw_create_table]
+    // Checks if table exists, creates table if does not exist.
+    if (!adminClient.exists(tableId)) {
+      System.out.println("Creating table: " + tableId);
+      CreateTableRequest createTableRequest =
+          CreateTableRequest.of(tableId).addFamily(COLUMN_FAMILY);
+      adminClient.createTable(createTableRequest);
+      System.out.printf("Table %s created successfully%n", tableId);
+    }
+    // [END bigtable_hw_create_table]
+  }
+
+  /** Demonstrates how to write some rows to a table. */
+  public void writeToTable() {
+    // [START bigtable_hw_write_rows]
+    try {
+      System.out.println("\nWriting some greetings to the table");
+      String[] names = {"World", "Bigtable", "Java"};
+      for (int i = 0; i < names.length; i++) {
+        String greeting = "Hello " + names[i] + "!";
+        RowMutation rowMutation =
+            RowMutation.create(tableId, ROW_KEY_PREFIX + i)
+                .setCell(COLUMN_FAMILY, COLUMN_QUALIFIER_NAME, names[i])
+                .setCell(COLUMN_FAMILY, COLUMN_QUALIFIER_GREETING, greeting);
+        dataClient.mutateRow(rowMutation);
+        System.out.println(greeting);
+      }
+    } catch (NotFoundException e) {
+      System.err.println("Failed to write to non-existent table: " + e.getMessage());
+    }
+    // [END bigtable_hw_write_rows]
+  }
+
+  /** Demonstrates how to read a single row from a table. */
+  public Row readSingleRow() {
+    // [START bigtable_hw_get_by_key]
+    try {
+      System.out.println("\nReading a single row by row key");
+      Row row = dataClient.readRow(tableId, ROW_KEY_PREFIX + 0);
+      System.out.println("Row: " + row.getKey().toStringUtf8());
+      for (RowCell cell : row.getCells()) {
+        System.out.printf(
+            "Family: %s    Qualifier: %s    Value: %s%n",
+            cell.getFamily(), cell.getQualifier().toStringUtf8(), cell.getValue().toStringUtf8());
+      }
+      return row;
+    } catch (NotFoundException e) {
+      System.err.println("Failed to read from a non-existent table: " + e.getMessage());
+      return null;
+    }
+    // [END bigtable_hw_get_by_key]
+  }
+
+  /** Demonstrates how to access specific cells by family and qualifier. */
+  public List<RowCell> readSpecificCells() {
+    // [START bigtable_hw_get_by_key]
+    try {
+      System.out.println("\nReading specific cells by family and qualifier");
+      Row row = dataClient.readRow(tableId, ROW_KEY_PREFIX + 0);
+      System.out.println("Row: " + row.getKey().toStringUtf8());
+      List<RowCell> cells = row.getCells(COLUMN_FAMILY, COLUMN_QUALIFIER_NAME);
+      for (RowCell cell : cells) {
+        System.out.printf(
+            "Family: %s    Qualifier: %s    Value: %s%n",
+            cell.getFamily(), cell.getQualifier().toStringUtf8(), cell.getValue().toStringUtf8());
+      }
+      return cells;
+    } catch (NotFoundException e) {
+      System.err.println("Failed to read from a non-existent table: " + e.getMessage());
+      return null;
+    }
+    // [END bigtable_hw_get_by_key]
+  }
+
+  /** Demonstrates how to read an entire table. */
+  public List<Row> readTable() {
+    // [START bigtable_hw_scan_all]
+    try {
+      System.out.println("\nReading the entire table");
+      Query query = Query.create(tableId);
+      ServerStream<Row> rowStream = dataClient.readRows(query);
+      List<Row> tableRows = new ArrayList<>();
+      for (Row r : rowStream) {
+        System.out.println("Row Key: " + r.getKey().toStringUtf8());
+        tableRows.add(r);
+        for (RowCell cell : r.getCells()) {
+          System.out.printf(
+              "Family: %s    Qualifier: %s    Value: %s%n",
+              cell.getFamily(), cell.getQualifier().toStringUtf8(), cell.getValue().toStringUtf8());
+        }
+      }
+      return tableRows;
+    } catch (NotFoundException e) {
+      System.err.println("Failed to read a non-existent table: " + e.getMessage());
+      return null;
+    }
+    // [END bigtable_hw_scan_all]
+  }
+
+  /** Demonstrates how to delete a table. */
+  public void deleteTable() {
+    // [START bigtable_hw_delete_table]
+    System.out.println("\nDeleting table: " + tableId);
+    try {
+      adminClient.deleteTable(tableId);
+      System.out.printf("Table %s deleted successfully%n", tableId);
+    } catch (NotFoundException e) {
+      System.err.println("Failed to delete a non-existent table: " + e.getMessage());
+    }
+    // [END bigtable_hw_delete_table]
+  }
+}
---
+++ src/TestBigtable.java
@@ -0,0 +1,159 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * Tests data loading and querying on Google Bigtable.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestBigtable {
+	/**
+	 * Class being tested
+	 */
+	private static Bigtable db;
+
+	/**
+	 * Requests a connection to the server.
+	 * 
+	 * @throws Exception
+	 *                   if an error occurs
+	 */
+	@BeforeAll
+	public static void init() throws Exception {
+		db = new Bigtable();
+		db.connect();
+	}
+
+	/**
+     * Tests connect
+     */
+    @Test
+	@Order(1)
+    public void testConnect() 
+    {   
+		try
+    	{ 	
+			db.connect();
+			if (db.dataClient == null || db.adminClient == null)
+				fail("Failed to connect");
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+			fail("Connection failed");
+		}		
+    }
+
+	
+	/**
+     * Tests load command.
+     */
+    @Test
+	@Order(3)
+    public void testLoad() 
+    {    		        	
+    	// See if data is loaded. Test would depend on schema.
+    	try
+    	{
+			// Verify row count to test load?
+			// Note: No current test for this.
+    	}
+    	catch (Exception e)
+    	{
+    		System.out.println(e);
+			fail(e.toString());
+    	}
+    }
+	
+	/**
+     * Tests first query.
+     */
+    @Test
+	@Order(4)
+    public void testQuery1() throws Exception
+    {        	    	
+    	int temp = db.query1();
+		System.out.println("Temperature: "+temp);
+    	assertEquals(52, temp);  		
+    }
+
+	/**
+     * Tests second query.
+     */
+    @Test
+	@Order(5)
+    public void testQuery2() throws Exception
+    {        	  
+		int windspeed = db.query2();
+		System.out.println("Windspeed: "+windspeed);
+		assertEquals(25, windspeed);  		
+    }
+
+	/**
+     * Tests third query.
+     */
+    @Test
+	@Order(6)
+    public void testQuery3() throws Exception
+    {        	    
+		ArrayList<Object[]> data = db.query3();
+
+		StringBuffer buf = new StringBuffer();
+
+		for (int i=0; i < data.size(); i++)
+		{
+			Object[] vals = data.get(i);
+
+			for (int j=0; j < vals.length; j++)
+				buf.append(vals[j].toString()+" ");
+			buf.append("\n");
+		}
+		System.out.println(buf.toString());
+		
+		String answer = "2022-10-02 00 74 53 47.8 9 1014.1 "
+						+"\n2022-10-02 01 69 53 56.7 7 1014.1 "
+						+"\n2022-10-02 02 67 53 60.7 7 1014.3 "
+						+"\n2022-10-02 03 66 53 62.9 7 1014.4 "
+						+"\n2022-10-02 04 64 53 67.4 7 1014.2 "
+						+"\n2022-10-02 05 63 52 67.3 7 1014.1 "
+						+"\n2022-10-02 06 61 52 72.2 8 1014.3 "
+						+"\n2022-10-02 07 63 51 64.8 9 1014.2 "
+						+"\n2022-10-02 08 61 53 74.9 4 1014 "
+						+"\n2022-10-02 09 59 52 77.5 0 1014.2 "
+						+"\n2022-10-02 10 58 52 80.4 0 1014.3 "
+						+"\n2022-10-02 11 55 51 86.3 3 1014.3 "
+						+"\n2022-10-02 12 57 52 83.3 4 1014.7 "
+						+"\n2022-10-02 13 56 52 86.4 3 1015.2 "
+						+"\n2022-10-02 14 57 52 83.3 0 1015.6 "
+						+"\n2022-10-02 15 62 53 72.3 5 1015.9 "
+						+"\n2022-10-02 16 66 53 62.9 8 1016.2 "
+						+"\n2022-10-02 17 70 53 54.8 5 1016.4 "
+						+"\n2022-10-02 18 72 54 53.1 3 1016.2 "
+						+"\n2022-10-02 19 76 52 43.1 6 1016 "
+						+"\n2022-10-02 20 77 53 43.3 5 1015.7 "
+						+"\n2022-10-02 21 78 53 41.9 5 1015.3 "
+						+"\n2022-10-02 22 79 52 39.1 5 1015.3 "
+						+"\n2022-10-02 23 79 51 37.6 4 1015.2 \n";
+						
+		assertEquals(answer, buf.toString());
+    }
+
+	/**
+     * Tests fourth query.
+     */
+    @Test
+	@Order(7)
+    public void testQuery4() throws Exception
+    {        	 
+		int temp = db.query4();
+		System.out.println("Temperature: "+temp);
+    	assertEquals(101, temp);     		
+    }
+}
